16b15660f27a4214e8f76bae66dd5039a40bfd62
Aaron Sullivan <aasullivan1618@gmail.com>
2024-04-21 14:14:08 -0700

    Making headway on first nui layout

--- lua/gpt/init.lua
+++ lua/gpt/init.lua
@@ -1,4 +1,5 @@
-local util = require('../util')
+local util = require('../lua.util')
+local edit_window = util.R("gpt.edit_window")
 
 local M = {}
 
@@ -13,8 +14,10 @@ end
 -- The main function
 ---@param opts { visual_mode: boolean }
 M.run = function(opts)
+
   if opts.visual_mode then
-    util.log(util.get_visual_selection())
+    local selected_text = util.get_visual_selection().text
+    edit_window.build_and_mount(selected_text)
   else
     util.log('not in visual mode')
   end
