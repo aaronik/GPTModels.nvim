81d8592ac8d820ab255ccc5aa8f908236a61f8e0
Aaron Sullivan <aasullivan1618@gmail.com>
2024-12-31 14:40:57 -0500

    Update init for development
    Make init file a little easier to toggle between development and normal
    mode

--- plugin/init.lua
+++ plugin/init.lua
@@ -2,26 +2,21 @@
 --
 -- via https://www.petergundel.de/neovim/lua/hack/2023/12/17/get-neovim-mode-when-executing-a-command.html
 
-local util = require('gptmodels.util')
+local function models()
+  -- For development only
+  -- local util = require('gptmodels.util')
+  -- return util.R('gptmodels')
+  return require('gptmodels')
+end
 
 function InvokeGptModelsCode(opts)
-  local gpt_opts = {
-    visual_mode = opts.count ~= -1
-  }
-
-  -- For development only
-  -- util.R('gptmodels').code(gpt_opts);
-  require('gptmodels').code(gpt_opts)
+  local gpt_opts = { visual_mode = opts.count ~= -1 }
+  models().code(gpt_opts)
 end
 
 function InvokeGptModelsChat(opts)
-  local gpt_opts = {
-    visual_mode = opts.count ~= -1
-  }
-
-  -- For development only
-  -- util.R('gptmodels').chat(gpt_opts);
-  require('gptmodels').chat(gpt_opts)
+  local gpt_opts = { visual_mode = opts.count ~= -1 }
+  models().chat(gpt_opts)
 end
 
 vim.api.nvim_create_user_command(
