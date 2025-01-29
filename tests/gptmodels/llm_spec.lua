3844ad5ec23c817083cba82fbc8082b46074b87d
Aaron Sullivan <aasullivan1618@gmail.com>
2025-01-28 17:39:53 -0800

    chore: Remove unnecessary level of indentation in llm_spec

--- tests/gptmodels/llm_spec.lua
+++ tests/gptmodels/llm_spec.lua
@@ -7,101 +7,98 @@ local Store = require('gptmodels.store')
 local ollama = require('gptmodels.providers.ollama')
 local openai = require('gptmodels.providers.openai')
 
-describe("gpt.llm", function()
-  describe("calling the provider stored in the Store", function()
-    local old_provider
-    local old_model
-    local ollama_chat_stub
-    local ollama_generate_stub
-    local openai_chat_stub
-    local openai_generate_stub
-    local snapshot
+describe("llm: calling the provider stored in the Store", function()
+  local old_provider
+  local old_model
+  local ollama_chat_stub
+  local ollama_generate_stub
+  local openai_chat_stub
+  local openai_generate_stub
+  local snapshot
 
-    before_each(function()
-      snapshot = assert:snapshot()
-      old_provider = Store:get_model().provider
-      old_model = Store:get_model().model
+  before_each(function()
+    snapshot = assert:snapshot()
+    old_provider = Store:get_model().provider
+    old_model = Store:get_model().model
 
-      ollama_chat_stub = stub(ollama, 'chat')
-      ollama_generate_stub = stub(ollama, 'generate')
-      openai_chat_stub = stub(openai, 'chat')
-      openai_generate_stub = stub(openai, 'generate')
-    end)
+    ollama_chat_stub = stub(ollama, 'chat')
+    ollama_generate_stub = stub(ollama, 'generate')
+    openai_chat_stub = stub(openai, 'chat')
+    openai_generate_stub = stub(openai, 'generate')
+  end)
 
-    after_each(function()
-      Store:set_model(old_provider, old_model)
-      snapshot:revert()
-    end)
+  after_each(function()
+    Store:set_model(old_provider, old_model)
+    snapshot:revert()
+  end)
 
-    it("for ollama/chat", function()
-      Store:set_model("ollama", "mawdle")
+  it("for ollama/chat", function()
+    Store:set_model("ollama", "mawdle")
 
-      llm.chat({
-        llm = {
-          stream = true,
-        },
-        on_read = function() end
-      })
+    llm.chat({
+      llm = {
+        stream = true,
+      },
+      on_read = function() end
+    })
 
-      assert.stub(ollama_chat_stub).was_called(1)
+    assert.stub(ollama_chat_stub).was_called(1)
 
-      ---@type MakeChatRequestArgs
-      local args = ollama_chat_stub.calls[1].refs[1]
-      assert.equal("mawdle", args.llm.model)
-    end)
+    ---@type MakeChatRequestArgs
+    local args = ollama_chat_stub.calls[1].refs[1]
+    assert.equal("mawdle", args.llm.model)
+  end)
 
-    it("for ollama/generate", function()
-      Store:set_model("ollama", "mawdle")
+  it("for ollama/generate", function()
+    Store:set_model("ollama", "mawdle")
 
-      llm.generate({
-        llm = {
-          prompt = "generate meh",
-          stream = true,
-        },
-        on_read = function() end
-      })
+    llm.generate({
+      llm = {
+        prompt = "generate meh",
+        stream = true,
+      },
+      on_read = function() end
+    })
 
-      assert.stub(ollama_generate_stub).was_called(1)
+    assert.stub(ollama_generate_stub).was_called(1)
 
-      ---@type MakeGenerateRequestArgs
-      local args = ollama_generate_stub.calls[1].refs[1]
-      assert.equal("mawdle", args.llm.model)
-    end)
+    ---@type MakeGenerateRequestArgs
+    local args = ollama_generate_stub.calls[1].refs[1]
+    assert.equal("mawdle", args.llm.model)
+  end)
 
-    it("for openai/chat", function()
-      Store:set_model("openai", "mawdle")
+  it("for openai/chat", function()
+    Store:set_model("openai", "mawdle")
 
-      llm.chat({
-        llm = {
-          stream = true,
-        },
-        on_read = function() end
-      })
+    llm.chat({
+      llm = {
+        stream = true,
+      },
+      on_read = function() end
+    })
 
-      assert.stub(openai_chat_stub).was_called(1)
+    assert.stub(openai_chat_stub).was_called(1)
 
-      ---@type MakeChatRequestArgs
-      local args = openai_chat_stub.calls[1].refs[1]
-      assert.equal("mawdle", args.llm.model)
-    end)
+    ---@type MakeChatRequestArgs
+    local args = openai_chat_stub.calls[1].refs[1]
+    assert.equal("mawdle", args.llm.model)
+  end)
 
-    it("for openai/generate", function()
-      Store:set_model("openai", "mawdle")
+  it("for openai/generate", function()
+    Store:set_model("openai", "mawdle")
 
-      llm.generate({
-        llm = {
-          prompt = "generate meh",
-          stream = true,
-        },
-        on_read = function() end
-      })
+    llm.generate({
+      llm = {
+        prompt = "generate meh",
+        stream = true,
+      },
+      on_read = function() end
+    })
 
-      assert.stub(openai_generate_stub).was_called(1)
+    assert.stub(openai_generate_stub).was_called(1)
 
-      ---@type MakeGenerateRequestArgs
-      local args = openai_generate_stub.calls[1].refs[1]
-      assert.equal("mawdle", args.llm.model)
-    end)
-
+    ---@type MakeGenerateRequestArgs
+    local args = openai_generate_stub.calls[1].refs[1]
+    assert.equal("mawdle", args.llm.model)
   end)
 end)
